d0a327b8197ae57e8c9d7e1ff7dba0d97ff36cbf
Deb <sandipan.deb@capgemini.com>
2019-09-25 09:41:32 +0100

    Added the CSVfromAPI script

---
+++ CSVfromAPI.ps1
@@ -0,0 +1,8 @@
+﻿$User = "<id>"
+$Token = "<Token>"
+
+$Uri = "<API>"
+$base64authinfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $User, $Token)))
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$response = Invoke-RestMethod -Method Get -ContentType application/json -Uri $Uri -Headers @{Authorization=("Basic {0}" -f $base64authinfo)}
+$response.value |  Export-Csv -Path "<Path for CSV>"
